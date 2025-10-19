d78f158933921783ec2d08e3171ca441d0b438f5
mutiarafassya <mutiara.fassya4113@student.unri.ac.id>
2025-10-19 15:52:17 +0700

    Menambahkan file master, index, dan memodifikasi isi folder layout

---
+++ resources/views/Admin/layout/header.blade.php
@@ -0,0 +1,7 @@
+<header class="bg-white shadow-md p-4 flex justify-between items-center">
+    <h1 class="text-xl font-semibold">Admin Panel</h1>
+    <div>
+        <span class="mr-3">Halo, Admin!</span>
+        <a href="{{ route('logout') }}" class="text-red-500 hover:underline">Logout</a>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ReservasiController;
+use App\Http\Controllers\Admin\TreatmentController;
+use App\Http\Controllers\Admin\FeedbackController;
+use App\Http\Controllers\Admin\FaqController;
+use App\Http\Controllers\RiwayatController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +20,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->name('admin.')->group(function () {
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('/reservasi', ReservasiController::class);
+Route::resource('/treatment', TreatmentController::class);
+Route::resource('/feedback', FeedbackController::class);
+Route::resource('/faq', FaqController::class);
+Route::resource('/riwayat', RiwayatController::class);
 });
+
