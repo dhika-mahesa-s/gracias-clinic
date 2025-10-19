23663685ca005d6753d1fa054a82090f51945722
mutiarafassya <mutiara.fassya4113@student.unri.ac.id>
2025-10-19 12:48:25 +0700

    Memodifikasi dashboard dan reservasi controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        // data untuk grafik
+        $totalReservasi = Reservasi::count();
+        $pendapatan = Reservasi::sum('harga');
+        $reservasiPerBulan = Reservasi::selectRaw('MONTH(created_at) as bulan, COUNT(*) as total')
+                                      ->groupBy('bulan')
+                                      ->pluck('total', 'bulan');
+
+        return view('admin.dashboard', compact('totalReservasi', 'pendapatan', 'reservasiPerBulan'));
+    }
 }
