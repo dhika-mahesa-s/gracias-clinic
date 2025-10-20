86decc7355881b050a6d1de17d76b44c26d13ba2
bimaiqrom <bima.iqrom5271@student.unri.ac.id>
2025-10-21 00:56:30 +0700

    menambahkan form registrasi

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,14 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
+// Register Routes
+use App\Http\Controllers\Auth\RegisterController;
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+
+
 // ==========================
 // FEEDBACK (User Side)
 // ==========================
