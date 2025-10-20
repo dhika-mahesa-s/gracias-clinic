7eef1e8eab98a2f018d909222488216701f7e94a
Dhika Mahesa S <dhikamahesa45@gmail.com>
2025-10-20 23:22:05 +0700

    feat(auth): setup Laravel Breeze authentication (login, register, password)

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,33 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\landingpagecontroller;
+use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', [LandingPageController::class, 'index'])->name('landingpage');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// ✅ tambahkan route reservasi di sini
+Route::middleware(['auth'])->group(function () {
+    Route::get('/reservasi', [ReservationController::class, 'index'])->name('reservasi.index');
+    Route::post('/reservasi', [ReservationController::class, 'store'])->name('reservasi.store');
+    Route::get('/reservasi/jadwal/{doctor}/{date}', [ReservationController::class, 'getSchedule']);
+});
+
+require __DIR__.'/auth.php';
