6983a6fe790fd91da1642b4f1dc2c2f54e241868
aidhatita3285 <aidha.tita3285@student.unri.ac.id>
2025-10-23 17:45:38 +0700

    build: update dependencies frontend dan konfigurasi Vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/css/app.css', // ganti dari sass ke css
                 'resources/js/app.js',
             ],
             refresh: true,
